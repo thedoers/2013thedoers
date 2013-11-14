9b98ef10e8cec9da8a681f587ddf1575f5ed4f1f
Piermaria Cosina <piermaria@thedoersproject.com>
2013-11-14 16:39:54 +0100

    new site cleanup

---
+++ config.rb
@@ -0,0 +1,58 @@
+activate :deploy do |deploy|
+    deploy.method = :ftp
+    deploy.host = "web332.webfaction.com"
+    deploy.user = "piermaria"
+    deploy.password = "Nonmelaricordo2"
+    deploy.path = "/home/piermaria/webapps/2013thedoers"
+    deploy.build_before = true
+end
+
+require 'builder'
+
+page "/", :layout => :html5
+page "/team.html", :layout => :html5
+page "/404.html", :layout => :html5
+
+page "/sitemap.xml", :layout => false
+
+
+
+
+require 'susy'
+
+
+set :css_dir, 'css'
+
+set :js_dir, 'js'
+
+set :images_dir, 'images'
+
+
+set :fonts_dir, 'fonts'
+
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+  
+  # # Minify Javascript on build
+  activate :minify_javascript
+  activate :minify_html
+  # # Create favicon/touch icon set from source/favicon_base.png
+  #activate :favicon_maker
+  
+  # # Enable cache buster
+  activate :cache_buster
+  #activate :image_optim
+  
+  # # Use relative URLs
+  #activate :relative_assets
+  # # To put width and height inside tag and to compression
+  activate :automatic_image_sizes
+  activate :gzip
+  #activate :smusher
+  
+  # Or use a different image path
+  #set :http_path, "http://thedoers.co/"
+end

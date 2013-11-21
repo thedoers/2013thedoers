d1d895baa0fe0a1031c54eb406a68dc0c98873dc
Piermaria Cosina <piermaria@thedoersproject.com>
2013-11-21 22:03:47 +0100

    initial js for subscribe

--- source/js/site.js
+++ source/js/site.js
@@ -1,12 +1,34 @@
 //= require "vendor/jquery"
 //= require "vendor/jquery.unveil.min"
-$( document ).ready(function() {
 
-$(".bottone-container-hover a").click(function(e){
+/**
+ * Created with JetBrains RubyMine.
+ * User: piermariacosina
+ * Date: 21/11/13
+ * Time: 19:32
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function(){
+    $( "#submit-btn" ).submit(function( event ) {
 
-	e.stopPropagation();
+            // Stop form from submitting normally
+            event.preventDefault();
+            console.log("prevent");
+            // Get some values from elements on the page:
+            var $form = $( this ),
+                email = $form.find( "input[name='email']" ).val(),
+                list = $form.find( "input[name='list']" ).val(),
+                listall = $form.find( "input[name='list all']" ).val(),
+                language = $form.find( "input[name='language']" ).val(),
+                url = $form.attr( "action" );
 
-	});
+            // Send the data using post
+            var posting = $.post( url, { email: email, list: list, listall:listall,language:language } );
 
-$("img").unveil(200);
+            // Put the results in a div
+            posting.done(function( data ) {
+                console.log(data);
+
+            });
+    });
 });

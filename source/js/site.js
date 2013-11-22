fb6deb72ed7202613a7e3088f007b3ccd5e4a7c0
Piermaria Cosina <piermaria@thedoersproject.com>
2013-11-22 10:45:30 +0100

    form js sendy + tag manager

--- source/js/site.js
+++ source/js/site.js
@@ -1,6 +1,6 @@
 //= require "vendor/jquery"
 //= require "vendor/jquery.unveil.min"
-
+//= require "subscribe"
 /**
  * Created with JetBrains RubyMine.
  * User: piermariacosina
@@ -9,26 +9,5 @@
  * To change this template use File | Settings | File Templates.
  */
 $(document).ready(function(){
-    $( "#submit-btn" ).submit(function( event ) {
-
-            // Stop form from submitting normally
-            event.preventDefault();
-            console.log("prevent");
-            // Get some values from elements on the page:
-            var $form = $( this ),
-                email = $form.find( "input[name='email']" ).val(),
-                list = $form.find( "input[name='list']" ).val(),
-                listall = $form.find( "input[name='list all']" ).val(),
-                language = $form.find( "input[name='language']" ).val(),
-                url = $form.attr( "action" );
-
-            // Send the data using post
-            var posting = $.post( url, { email: email, list: list, listall:listall,language:language } );
-
-            // Put the results in a div
-            posting.done(function( data ) {
-                console.log(data);
-
-            });
-    });
+    ajaxform_subscribe();
 });
---
+++ source/js/subscribe.js
@@ -0,0 +1,65 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: piermariacosina
+ * Date: 22/11/13
+ * Time: 09:49
+ * To change this template use File | Settings | File Templates.
+ */
+function ajaxform_subscribe(){
+    $("#signup-form").submit(function(e){
+        e.preventDefault();
+
+        var $form = $(this),
+            name = $form.find('input[name="name"]').val(),
+            email = $form.find('input[name="email"]').val(),
+            list = $form.find('input[name="list"]').val(),
+            url = $form.attr('action');
+
+        $.post(url, {name:name, email:email, list:list, boolean:true},
+            function(data) {
+                if(data)
+                {
+                    if(data=="Some fields are missing.")
+                    {
+                        $("#status").text("Please fill in your name and email.");
+                        $("#status").css("color", "red");
+                    }
+                    else if(data=="Invalid email address.")
+                    {
+                        $("#status").text("Your email address is invalid.");
+                        $("#status").css("color", "red");
+                    }
+                    else if(data=="Invalid list ID.")
+                    {
+                        $("#status").text("Your list ID is invalid.");
+                        $("#status").css("color", "red");
+                    }
+                    else if(data=="Already subscribed.")
+                    {
+                        $("#status").text("You're already subscribed!");
+                        $("#status").css("color", "red");
+                    }
+                    else
+                    {
+                        $("#status").text("You're subscribed!");
+                        $("#status").css("color", "green");
+                    }
+                }
+                else
+                {
+                    alert("Sorry, unable to subscribe. Please try again later!");
+                }
+            }
+        );
+    });
+    $("#signup-form").keypress(function(e) {
+        if(e.keyCode == 13) {
+            e.preventDefault();
+            $(this).submit();
+        }
+    });
+    $("#submit-btn").click(function(e){
+        e.preventDefault();
+        $("#signup-form").submit();
+    });
+}

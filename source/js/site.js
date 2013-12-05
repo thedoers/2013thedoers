c5dd4585b19f289e8c15426463713b6f360d92c3
Piermaria Cosina <piermaria@thedoersproject.com>
2013-12-05 14:46:25 +0100

    scrollto functionality

--- source/js/site.js
+++ source/js/site.js
@@ -2,7 +2,7 @@
 //= require "subscribe"
 //= require "vendor/jquery.easing.min"
 //= require "vendor/jquery.fadeloader"
-//= require "vendor/jquery.isotope.min"
+//= require "vendor/jquery.scrollTo"
 //=require "passURL"
 /**
  * Created with JetBrains RubyMine.
@@ -11,21 +11,44 @@
  * Time: 19:32
  * To change this template use File | Settings | File Templates.
  */
+
+
 $(document).ready(function(){
     overlay("a.map");
     getQueryVars("http://0.0.0.0:4567/events/2013/thank-you-ixdt1113.html");
     getQueryVars("http://0.0.0.0:4567/workshops/2013/thank-you-workshop1213.html");
     ajaxform_subscribe("#signup-form-wp1213","#status-wp1213");
     ajaxform_subscribe("#signup-form-ixdt1113","#status-ixdt1113");
+    $.scrollTo(elem);
 
-    $('#container').fadeloader({mode: "children",preloadImg: '/loading.gif',
+    $('#container').fadeloader({
+
+        mode: "children",
+        preloadImg: '/loading.gif',
         preloadWidth: 36,
         preloadHeight: 36,
-        displayType: "inline"});
+        displayType: 'inline',
+        onComplete: "scroll()"
+        });
+
+    if (window.location.hash) {
+        var params = (window.location.hash.substr(1)).split("&");
+
+        for (i = 0; i < params.length; i++)
+        {
+            var a = params[i].split("=");
+
+            var elem = $( window.location.hash.replace('#_', '#'));
+
+
+        }
+    }
 });
 
 
-
+function scroll(){
+    $.scrollTo("#wp1213",400);
+}
 function overlay(element){
     $(element).hover(function(){
         $(this).find(".overlay").fadeOut(400);

eb11b4c5ffc49571ba50d6fd05a5da50e04c0f8f
drmartinez <danny.martinez@neagen.it>
2013-12-06 17:43:20 +0100

    Fabrica grid

--- source/js/site.js
+++ source/js/site.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 //        mp4:'js/videos/background_07052013.mp4',
         webm: 'js/videos/s01.webm',
         ogv: 'js/videos/s01.ogv',
-        poster: 'js/videos/tunnel_animation.jpg',
+//        poster: 'js/videos/tunnel_animation.jpg',
 //        poster:'js/videos/background_07052013.png',
         width: "100%",
         position: "relative",

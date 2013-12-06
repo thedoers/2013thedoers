6b42b056e6743c0caaaf5bf6705417946a574508
drmartinez <danny.martinez@neagen.it>
2013-12-06 12:08:44 +0100

    Fabrica i video + videoBg plugin

--- source/js/site.js
+++ source/js/site.js
@@ -21,11 +21,17 @@ $(document).ready(function(){
     ajaxform_subscribe("#signup-form-wp1213","#status-wp1213");
     ajaxform_subscribe("#signup-form-ixdt1113","#status-ixdt1113");
     $('#video').videoBG({
-        mp4:'videos/background_07052013.mp4',
-
-        poster:'videos/background_07052013.png',
+        mp4: 'js/videos/tunnel_animation.mp4',
+//        mp4:'js/videos/background_07052013.mp4',
+        webm: 'js/videos/tunnel_animation.webm',
+        ogv: 'js/videos/tunnel_animation.ogv',
+        poster: 'js/videos/tunnel_animation.jpg',
+//        poster:'js/videos/background_07052013.png',
+//        width: "100%",
+//        height: "auto",
+        position: "relative",
         scale:true,
-        zIndex:0
+        zIndex:-1
     });
 
     $('#container').fadeloader({
